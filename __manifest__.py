7a8f582ab333a9f69dfb9ebaa2168e9c34fa40a2
Musaed-saif <137612238+Musaed-saif@users.noreply.github.com>
2023-06-24 15:38:41 +0300

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+  'name': 'Car pro',
+  'version': '1.0',
+  'category': 'Accounting/Accounting',
+  'author': 'fatema basaheeh',
+  'website': "",
+  'company': 'fatema basaheeh',
+  'maintainer': 'fatema basaheeh',
+  'summary': 'Record Car Details',
+  "description": "",
+  'depends': ['base'],
+   'data': [
+      'security/car_pro.csv',
+      'views/car_pro_views.xml',
+  ],
+  'installable':True,
+  'application':True,
+  'license':'LGPL-3',
+ 
+}
